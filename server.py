ecce21817740fcd7ebf1e40d79a66abf5ab597d8
Grace Chung <gcw.chung@gmail.com>
2020-03-31 17:38:11 -0700

    Added email & pw validation, storing new user data

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ from jinja2 import StrictUndefined
 
 app = Flask(__name__)
 
-
 app.jinja_env.undefined = StrictUndefined
 
 @app.route('/')
@@ -22,6 +21,29 @@ def show_signup_form():
 @app.route('/signup', methods=["POST"])
 def process_signup():
     """Stores user registration data in db, redirect to homepage"""
+
+    username = request.form.get('username').title()
+    email =  request.form.get('email')
+    pw = request.form.get('pwd')
+
+    # Validate/Check Username in DB
+    if User.query.filter(User.username == username).first():
+        flash("Username already exists.")
+        return redirect('/signup')
+    # Check Email in DB
+    if User.query.filter(User.email == email).first():
+        flash("Email already exists")
+        return redirect('/signup')
+    else:
+        current_date = date.today()
+        signup_date = current_date.strftime("%Y-%b-%d")
+        # Store username & pw in DB
+        db.session.add(User(username=username, 
+                            email=email.lower(),
+                            pw=pw,
+                            signup_date=signup_date))
+        db.session.commit()
+
     return redirect('/')
 
 
@@ -34,6 +56,18 @@ def show_login_form():
 @app.route('/login', methods=["POST"])
 def process_login():
     """Queries database, redirects to dashboard"""
+
+    username = request.form.get("username")
+    pw = request.form.get("password")
+    username=username.title()
+    # Query for username & pw in DB
+    user = User.query.filter_by(username=username, pw=pw).first()
+    if not user:
+        flash("Invalid Username or Passord."
+        return redirect(/login))
+    
+    session['username'] = user.username
+    
     return redirect('/user')
 
 
@@ -44,6 +78,7 @@ def user_login():
     return render_template("dashboard.html")
     
 
+##########################
 if __name__ == '__main__':
     # connect_to_db(app)
     DebugToolbarExtension(app)
