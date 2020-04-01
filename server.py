f4f830faf64e0bb936b773cd58d38bdf154de10d
Grace Chung <gcw.chung@gmail.com>
2020-03-31 18:47:18 -0700

    Added datetime

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 
+from datetime import date
 from model import connect_to_db, db, User
 
 app = Flask(__name__)
