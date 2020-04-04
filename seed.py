a1fa7714c87d57188f351658b14d967125cab973
Grace Chung <gcw.chung@gmail.com>
2020-04-03 18:10:08 -0700

    Added function to get confirmed reports of COVID19

--- seed.py
+++ seed.py
@@ -1,1 +1,17 @@
 """Parse data from API and store into database"""
+
+from pprint import pprint
+# make json file more readable
+import json
+# Needed to make API Requests
+import requests
+# No API key needed
+URL = "https://api.covid19api.com/country/us/status/confirmed"
+URL2 = "https://api.covid19api.com/country/us/status/deaths"
+
+def get_confirmed_reports():
+    """Returns confirmed reports of COVID-19 in each state."""
+
+    r1 = requests.get(URL)
+    return r1
+get_confirmed_reports()
