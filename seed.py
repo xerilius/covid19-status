b1b454e4ab401ce95dcfd498f0c92937006797f1
Grace Chung <gcw.chung@gmail.com>
2020-04-03 21:40:38 -0700

    Initial copy of api data

--- seed.py
+++ seed.py
@@ -3,15 +3,49 @@
 from pprint import pprint
 # make json file more readable
 import json
-# Needed to make API Requests
-import requests
+# Needed to make API Requests, threading, etec
+import requests, os, bs4, threading, time
 # No API key needed
 URL = "https://api.covid19api.com/country/us/status/confirmed"
 URL2 = "https://api.covid19api.com/country/us/status/deaths"
 
-def get_confirmed_reports():
-    """Returns confirmed reports of COVID-19 in each state."""
+# Read Only (‘r’): Open text file for reading.
+# Write Only (‘w’): Open the file for writing.
+# Append Only (‘a’): Open the file for writing. The data being written will be inserted at the end, after the existing data.
+# Read and Write (‘r+’): Open the file for reading and writing.
 
+
+def get_covid_status():
+    """Writes confirmed/death reports of COVID-19 in each state to txt file."""
     r1 = requests.get(URL)
-    return r1
-get_confirmed_reports()
+    r2 = requests.get(URL2)
+
+    # Confirmed
+    # Convert JSON data to python data structure
+    confirmed_data = json.loads(r1.text)
+    out_file = open("confirmed-reports.txt", "w")
+    json.dump(confirmed_data, out_file, indent=6)
+    out_file.close()
+
+    # Deaths
+    deaths_data = json.loads(r2.text)
+    out_file = open("death-reports.txt", "w")
+    json.dump(deaths_data, out_file, indent=6)
+    out_file.close()
+
+
+api_requests = []
+for i in range(0, 7):   # loop 7 times
+    callAPI = threading.Thread(target=get_covid_status)
+    api_requests.append(callAPI)  # keep track of thread object created
+    callAPI.start()
+    print("starting") 
+    time.sleep(86400)   # pause for 24 hrs
+    print()
+    print('Reeee')
+
+# Notify when all threads end
+for call_api in api_requests:
+    call_api.join()
+
+print('Completed tasks for the week!')
