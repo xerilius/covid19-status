64d1974c188e680000492f211d61e367eaf89cf1
Grace Chung <gcw.chung@gmail.com>
2020-04-06 22:27:09 -0700

    Fixed bug that prevented data to be inserted into database

--- seed.py
+++ seed.py
@@ -1,21 +1,16 @@
 """Parse data from API and store into database"""
 
 from datetime import datetime
-# make json file more readable
-import json
-# Needed to make API Requests, threading, etec
+from model import connect_to_db, db, Status, City
 import requests, os, bs4, threading, time
-
-from model import connect_to_db, db, Status, City, State_
+import json
 
 # No API key needed
 URL = "https://api.covid19api.com/country/us/status/confirmed"
 URL2 = "https://api.covid19api.com/country/us/status/deaths"
 
-def create_status():
-    """Organize data before transferring into db"""
-
-    # Retrieve data thru json file
+def enter_table_data():
+       # Retrieve data thru json file
     with open('confirmed.json', 'r') as outfile:
         api_data = json.load(outfile)
 
@@ -26,32 +21,14 @@ def create_status():
         'date': None,
     }
 
-    states_ = {}
-    cities_ = {}
-    db_cities = {}
-
-    for dict_ in api_data:
-        state = dict_['Province']
-        states_[state] = state
-
-        city = dict_.get('City', "None")
-        cities_[city] =  state
-
-    # Adding all states
-    for key in states_.keys():
-        usa_states = State_(state_name = key)
-        db.session.add(usa_states)
-        db.session.commit()
-
-    # Adding all cities
-    for key in cities_.keys():
-        db_cities[key] = City(city_name = key)
-  
-   ########################################## 
+    city_seen = {}
 
     for dict_ in api_data:
         city = dict_.get('City', "None")
         status_data['city'] = city
+
+        state = dict_['Province']
+        status_data['state'] = state
 
         case = dict_['Cases']
         status_data['case'] = case
@@ -65,71 +42,33 @@ def create_status():
             status_date=status_data['date'],
         )
         
-    
-        ree = db_cities[city]
-        reee = status.city_status.append(ree)
+        if status_data['city'] not in city_seen:
+            city_inst = City(city_name = status_data['city'], 
+                             state_name=status_data['state'])
+            add_to_citystatus = status.city_status.append(city_inst)
+            city_seen[status_data['city']] = status_data['city']
 
         db.session.add(status)
         db.session.commit()
 
-
-    # for dict_ in dicts:
-    #     state = dict_['Province']
-    #     status_data['state'] = state
-    #     states_[state] = state
-        
-    #     city = dict_.get('City',"None")
-    #     status_data['city'] = city
-    #     cities_[city] =  city
-
-    #     case = dict_['Cases']
-    #     status_data['case'] = case
-
-    #     date = dict_['Date']
-    #     date = datetime.strptime(date[0:10], '%Y-%m-%d')
-    #     status_data['date'] = date
-        
-    
-        # status = Status(
-        #     confirmed=int(status_data['case']),
-        #     status_date=status_data['date'],
-        # )
-        # status.city_status.append(City(city_name=status_data['city']))
-        # usa_states = State_(state_name = states_[state])
-        # db.session.add(status) 
-        # db.session.commit()
-
-
-
-
-        # AttributeError: 'State_' object has no attribute 'status'
-            # usa_states.status.append(State_(state_name=status_data['state']))
-
-  
-    # usa_states = State_(state_name = states_[state])
-    # usa_states.cities.append(City(city_name=cities_[city]))
-    # db.session.add(usa_states)
-    # db.session.commit()
-
-
-    # print(f'Created {status}')
+    print(f"Successfully created {status} and {city_inst}")
 
 
 def write_confirmed_data():
-    """Writes confirmed/death reports of COVID-19 in each state to txt file."""
+    """Writes confirmed reports of COVID-19 in each state to txt file."""
     r1 = requests.get(URL)
-    # # Method 1
-    # with open('confirmed-reports.txt', 'w') as f:
-    #     f.write(r1.text)
-
-    # Method 2
     data_ = json.loads(r1.text)
     with open('confirmed-reports.json', 'w') as outfile:
         json.dump(data_, outfile, indent=4)
     print('task done')
 
+    # # Method 2
+    # with open('confirmed-reports.txt', 'w') as f:
+    #     f.write(r1.text)
+
 
 def write_death_data(json):
+    """Writes death reports of COVID-19 in each state to txtfile."""
     r2 = requests.get(URL2)
     with open('deaths-reports.txt', 'w') as f:
         f.write(r2.text)
@@ -140,42 +79,41 @@ def write_death_data(json):
     # json.dump(deaths_data, out_file, indent=4)
     # out_file.close()
 
+
+
 if __name__ == "__main__":
     from server import app
     import os
+
     os.system("dropdb covid19")
     os.system("createdb covid19")
 
     connect_to_db(app)
-    # Make tables
     db.create_all()
-
-    # Enter data into DB
-    create_status()
+    enter_table_data()
 
 
+    def run_task():
+        """Update and rewrite data from API"""
+        for i in range(0,1):
+            t1 = threading.Thread(target=get_confirmed_status, args=(confirmed_txt,))
+            t2 = threading.Thread(target=get_death_status, args=(deaths_txt,))
+    
+            t1.start()
+            print("t1 started")
+            t2.start()
+            print("t2 started")
 
-    # def run_task():
-    #     """Run task 7 times"""
-    #     for i in range(0,1):
-    #         t1 = threading.Thread(target=get_confirmed_status, args=(confirmed_txt,))
-    #         t2 = threading.Thread(target=get_death_status, args=(deaths_txt,))
-    #         # start thread 
-    #         t1.start()
-    #         print("t1 started")
-    #         t2.start()
-    #         print("t2 started")
+            # time.sleep(1440) 
 
-    #         # time.sleep(1440)   
-    #         # wait until threads are completely executed
-    #         t1.join()
-    #         t2.join()
-    #         # both threads are completely executed
-    #         print("Tasks are completed")
+            # wait until threads are completely executed
+            t1.join()
+            t2.join()
+
+            print("Tasks are completed")
     # run_task()
 
 
-####################################   
 # # To seed data directly into DB without saving data into a file 
     # response = requests.get(URL)
     # response.raise_for_status()
