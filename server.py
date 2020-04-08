61f5ff709a54d4b4b9d09be34c1d5bfc958a218d
Grace Chung <gcw.chung@gmail.com>
2020-04-08 08:25:22 -0700

    D3 chart now displays data

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-
-from datetime import date
+from sqlalchemy import asc, desc
 from model import connect_to_db, db, User, Status, City
+
+import json
+from datetime import date
 
 app = Flask(__name__)
 # # set a 'SECRET_KEY' to enable the Flask session cookies
@@ -24,6 +26,7 @@ def index():
 @app.route('/signup', methods=["GET"])
 def show_signup_form():
     """Displays signup form"""
+
     return render_template("signup.html")
 
 
@@ -60,17 +63,17 @@ def process_signup():
 @app.route('/login', methods=["GET"])
 def show_login_form():
     """Displays login form"""
+
     return render_template("login.html")
 
 
 @app.route('/login', methods=["POST"])
 def process_login():
     """Queries database, redirects to dashboard"""
-    # request.form.get(name-field-of-form-input)
+
     username = request.form.get("username")
     pw = request.form.get("pwd")
     username = username.lower()
-    # Query for username & pw in DB
     user = User.query.filter_by(username=username, pw=pw).first()
     if not user:
         flash("Invalid Username or Password.")
@@ -85,24 +88,37 @@ def process_login():
 @app.route('/logout')
 def logout():
     """Logout user"""
+
     del session['username']
     flash("You successfully logged out")
     return redirect('/')
 
 
-# SEARCHBAR
-
+# SEARCH RESULTS
 @app.route('/search-results', methods=["POST"])
 def show_results():
     """Displays city from search result"""
-
     city_search = request.form.get("searchbar")
     print(city_search)
     search = "%{}%".format(city_search).title().strip()
-    city_names = City.query.filter(City.city_name.ilike(search)).all()
+    city_name = City.query.filter(City.city_name.ilike(search)).first()
+    state_name = city_name.state_name
+    city_id = city_name.city_id
+
+    # Get Recent 10 Records 
+    status10 = db.session.query(Status).filter(Status.city_id == city_id).order_by(desc(Status.status_id)).limit(10)
+
+    datasets = []
+    for item in status10:
+        datasets.append({
+            'date': str(item.status_date), 
+            'num': item.confirmed
+        })
+
+        data = json.dumps({"data":datasets})
     
-    return render_template('search_results.html', cities=city_names)
- 
+    return render_template('search_results.html', cities=city_name,
+    states=state_name, status10=status10, data=data)
 
 # DASHBOARD
 @app.route('/user/<username>', methods=["GET"])
@@ -120,12 +136,9 @@ def show_dashboard(username):
     return render_template("dashboard.html", 
                             current_date=current_date,
                             username=user_name)
-# check session username
-# query for favs
-# query for data
-    
 
-##########################
+
+
 if __name__ == '__main__':
     connect_to_db(app)
     DebugToolbarExtension(app)
