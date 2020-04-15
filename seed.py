4c564bde4d92ea1482ca7ec5344de122b1605ecb
Grace Chung <gcw.chung@gmail.com>
2020-04-15 02:11:36 -0700

    Added function to seed data directly from api response

--- seed.py
+++ seed.py
@@ -1,26 +1,101 @@
 """Parse data from API and store into database"""
 
 from datetime import datetime
-from model import connect_to_db, db, Status, City
+from model import connect_to_db, db, Status, County
 import requests, os, bs4, threading, time
 import json
+
 
 # No API key needed
 URL = "https://api.covid19api.com/country/us/status/confirmed"
 URL2 = "https://api.covid19api.com/country/us/status/deaths"
 
 
+def seed_data_directly_from_api():
+    """Parsing JSON directly from API and seeding into database. 
+        No written files involved."""
+
+    confirmed_response = requests.get(URL)
+    # fatality_response = requests.get(URL2)
+
+    dataset_confirmed = json.loads(confirmed_response.text)
+    # dataset_fatality = json.loads(fatality_response.text)
+
+    status_data = {
+        'state': None,
+        'city': None,
+        'confirmed': None,
+        'date': None,
+        'city_id': None,
+    }
+
+    city_seen = {}
+    db_cities = {"Unassigned": 0}
+    i = 1
+
+    for dict_ in dataset_confirmed:  
+        if dict_['City']:
+            city = dict_['City']
+            status_data['city'] = city
+
+        state = dict_['Province']
+        status_data['state'] = state
+
+        if status_data['city'] not in city_seen:
+            if status_data['city'] == "Unassigned":
+                i += 0
+            else: 
+                county_inst = County(county_name = status_data['city'], 
+                                    state_name=status_data['state'])
+                city_seen[status_data['city']] = status_data['city']
+                db_cities[status_data['city']] = i 
+                i += 1
+            db.session.add(county_inst)       
+        db.session.commit()    
+    print(f"Successfully created {county_inst}")
+
+    city_seen = {} 
+
+    # Create Status Table
+    for dict_ in dataset_confirmed:
+        if dict_['City']:
+            city = dict_['City']
+            status_data['city'] = city
+        
+        if city in db_cities:
+            status_data['city_id'] = db_cities[city]
+
+        state = dict_['Province']
+        status_data['state'] = state
+
+        case = dict_['Cases']
+        status_data['case'] = case
+
+        date = dict_['Date']
+        date = datetime.strptime(date[0:10], '%Y-%m-%d')
+        status_data['date'] = date
+
+        status = Status(
+            confirmed=int(status_data['case']),
+            status_date=status_data['date'],
+            county_id=int(status_data['city_id']),
+            state_name=status_data['state']
+        )
+        db.session.add(status)
+        db.session.commit()
+
+#############################################################
+
 def read_json():
     """Reads json file"""
 
     with open('confirmed-reports.json', 'r') as outfile:
         api_data = json.load(outfile)
-
     return api_data
 
 
-def create_city_ids():
-    """Returns dictionary of city_ids"""
+def create_county_ids():
+    """Returns dictionary of county_ids"""
 
     api_data = read_json()
     
@@ -43,12 +118,18 @@ def create_city_ids():
                 city_seen[status_data['city']] = status_data['city']
                 db_cities[status_data['city']] = i 
                 i += 1
-
     return db_cities
-    
 
-def enter_city_data():
+
+def run_all_json_files():
+    """Reads all JSON files"""
+    enter_county_data()
+    enter_confirmed_data()
+  
+
+def enter_county_data():
     """Inserts data for the City table"""
+
     api_data = read_json()
     db_cities = create_city_ids()
 
@@ -68,61 +149,60 @@ def enter_city_data():
 
         if status_data['city'] not in city_seen:
             if status_data['city'] != "Unassigned":
-                city_inst = City(city_id=db_cities[status_data['city']],
-                                city_name=status_data['city'],
+                county_inst = County(county_id=db_cities[status_data['city']],
+                                county_name=status_data['city'],
                                 state_name=status_data['state']
                                 )
-                db.session.add(city_inst)
+                db.session.add(county_inst)
             city_seen[status_data['city']] = db_cities[status_data['city']]
     db.session.commit()
 
                   
-# def enter_confirmed_data():
-#     """Inserts data for the confirmed status table"""
+def enter_confirmed_data():
+    """Inserts data for the confirmed status table"""
 
-#     api_data = read_json()
-#     db_cities = create_city_ids()
+    api_data = read_json()
+    db_cities = create_city_ids()
 
-#     status_data = {
-#         'state': None,
-#         'city': None,
-#         'confirmed': None,
-#         'date': None,
-#         'city_id': None,
-#     }
+    status_data = {
+        'state': None,
+        'city': None,
+        'confirmed': None,
+        'date': None,
+        'city_id': None,
+    }
 
-#     city_seen = {}
+    city_seen = {}
 
-#     # Create Status Table
-#     for dict_ in api_data:
-#         city = dict_.get('City', "None")
-#         status_data['city'] = city
-#         if city in db_cities:
-#             status_data['city_id'] = db_cities[city]
+    # Create Status Table
+    for dict_ in api_data:
+        city = dict_.get('City', "None")
+        status_data['city'] = city
+        if city in db_cities:
+            status_data['city_id'] = db_cities[city]
 
-#         state = dict_['Province']
-#         status_data['state'] = state
+        state = dict_['Province']
+        status_data['state'] = state
 
-#         case = dict_['Cases']
-#         status_data['case'] = case
+        case = dict_['Cases']
+        status_data['case'] = case
 
-#         date = dict_['Date']
-#         date = datetime.strptime(date[0:10], '%Y-%m-%d')
-#         status_data['date'] = date
+        date = dict_['Date']
+        date = datetime.strptime(date[0:10], '%Y-%m-%d')
+        status_data['date'] = date
 
-#         status = Status(
-#             confirmed=int(status_data['case']),
-#             status_date=status_data['date'],
-#             city_id=int(status_data['city_id']),
-#             state_name=status_data['state']
-#         )
-#         db.session.add(status)
-#         db.session.commit()
+        status = Status(
+            confirmed=int(status_data['case']),
+            status_date=status_data['date'],
+            county_id=int(status_data['city_id']),
+            state_name=status_data['state']
+        )
+        db.session.add(status)
+    db.session.commit()
 
-#     # print(f"Successfully created {status}")
+    print(f"Successfully created {status}")
 
-
-
+    
 def run_writing_tasks():
     """Update and rewrite data from API"""
     for i in range(0, 1):
@@ -148,6 +228,7 @@ def write_confirmed_data():
     """Writes confirmed reports of COVID-19 in each state to jsonile."""
     r1 = requests.get(URL)
     confirmed_data = json.loads(r1.text)
+
     with open('confirmed-reports.json', 'w') as outfile:
         json.dump(confirmed_data, outfile, indent=4)
     print('Task#1 completed.')
@@ -166,10 +247,8 @@ def write_fatality_data():
     # # Method 2
     # with open('fatality-reports.json', 'w') as outfile:
     #     outfile.write(r2.text)
-    # print('Task#2 completed.')
 
     # # Method 3
-    # fatality_data = json.loads(r2.text)
     # out_file = open(json, "w")
     # json.dump(fatality_data, out_file, indent=4)
     # out_file.close()
@@ -179,7 +258,7 @@ if __name__ == "__main__":
     from server import app
     import os
 
-    run_writing_tasks()
+    # run_writing_tasks()
 
     os.system("dropdb covid19")
     os.system("createdb covid19")
@@ -187,16 +266,13 @@ if __name__ == "__main__":
     connect_to_db(app)
     db.create_all()
     
-    db.session.add(City(city_id=0, city_name="Unassigned", state_name="TBD"))
+    db.session.add(
+        County(county_id=0, 
+            county_name="Unassigned", 
+            state_name="TBA"))
     db.session.commit()
-    enter_city_data()
-    # enter_table_data()
 
+    seed_data_directly_from_api()
+    # run_all_json_files()
     
 
-
-# # To seed data directly into DB without saving data into a file 
-    # response = requests.get(URL)
-    # response.raise_for_status()
-    # data_= json.loads(response.text)
-    # w = data_
