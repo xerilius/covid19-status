b8afca19b155d92fbeaa0ad93316563769fc1fed
Grace Chung <gcw.chung@gmail.com>
2020-04-16 06:38:54 -0700

    Added missing User table in import

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy import asc, desc
-from model import connect_to_db, db, County, Fatality, Confirmed
+from model import connect_to_db, db, County, Fatality, Confirmed, User
 
 import json
 from datetime import date
