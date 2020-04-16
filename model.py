3266f2ed1ff6e85d4959bab60d1bcea645fbc653
Grace Chung <gcw.chung@gmail.com>
2020-04-16 04:57:23 -0700

    Rearranged table order

--- model.py
+++ model.py
@@ -16,9 +16,8 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     pw = db.Column(db.String(50), nullable=False)
     
-
-    # saves = db.relationship("City", secondary="saves",
-    #                                 backref="users")
+    saves = db.relationship("City", secondary="saves",
+                                    backref="users")
 
     def __repr__(self):
         """Prints user information"""
@@ -27,47 +26,6 @@ class User(db.Model):
         )
 
 
-
-class Confirmed(db.Model):
-    """Confirmed number of people tested positive for COVID"""
-
-    __tablename__ = "confirmed"
-
-    confirmed_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    date = db.Column(db.Date, nullable=False)
-    county_id = db.Column(db.Integer, db.ForeignKey('counties.county_id'), nullable=False)
-    state_name = db.Column(db.String(64), nullable=True) 
-    confirmed = db.Column(db.Integer, nullable=False)
-    
-    # Association relationship for city
-    county_confirmed = db.relationship("County", backref="confirmed")
-
-
-    def __repr__(self):
-        """Provides info when printed"""
-        return "<Confirmed status_id={} status_date={} confirmed={}>".format(
-            self.confirmed_id, self.date, self.confirmed
-        )
- 
-
-class Fatality(db.Model):
-    """Information on fatalities from COVID"""
-
-    __tablename__ = "fatalities"
-
-    fatality_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    date = db.Column(db.Date, nullable=False)
-    county_id = db.Column(db.Integer, 
-                          db.ForeignKey('counties.county_id'), nullable=False)
-    state_name = db.Column(db.String(64), nullable=True) 
-    fatalities = db.Column(db.Integer, nullable=False)
-
-    # Association relationship for city
-    county_fatality = db.relationship("County", backref="fatalities")
-
-    def __repr__(self):
-        """Provides info when printed"""
-        return "<Fatality fatality_id={} fatality_date={} fatalities={}>".format(self.fatality_id, self.date, self.fatalities)
 
 
 class County(db.Model):
@@ -88,6 +46,7 @@ class County(db.Model):
         )
 
 
+
 class Save(db.Model):
     """Counties saved by user."""
 
@@ -98,10 +57,54 @@ class Save(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     county_id = db.Column(db.Integer, db.ForeignKey("counties.county_id"))
 
-
     def __repr__(self):
         """Provide save info when printed."""
         return f"<Save save_id={self.save_id}, user_id={self.user_id}, county_id={self.county_id}>"
+
+
+
+
+class Confirmed(db.Model):
+    """Confirmed number of people tested positive for COVID"""
+
+    __tablename__ = "confirmed"
+
+    confirmed_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    county_id = db.Column(db.Integer, db.ForeignKey('counties.county_id'), nullable=False)
+    state_name = db.Column(db.String(64), nullable=True) 
+    confirmed = db.Column(db.Integer, nullable=False)
+    
+    # Association relationship for city
+    county_confirmed = db.relationship("County", backref="confirmed")
+
+    def __repr__(self):
+        """Provides info when printed"""
+        return "<Confirmed status_id={} status_date={} confirmed={}>".format(
+            self.confirmed_id, self.date, self.confirmed
+        )
+ 
+
+
+class Fatality(db.Model):
+    """Information on fatalities from COVID"""
+
+    __tablename__ = "fatalities"
+
+    fatality_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    county_id = db.Column(db.Integer, 
+                          db.ForeignKey('counties.county_id'), nullable=False)
+    state_name = db.Column(db.String(64), nullable=True) 
+    fatalities = db.Column(db.Integer, nullable=False)
+
+    # Association relationship for city
+    county_fatality = db.relationship("County", backref="fatalities")
+
+    def __repr__(self):
+        """Provides info when printed"""
+        return "<Fatality fatality_id={} fatality_date={} fatalities={}>".format(self.fatality_id, self.date, self.fatalities)
+
 
 
 
