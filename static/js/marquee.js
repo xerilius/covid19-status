1604845d0a21d6d4dafe310f7582cde7ff60f77d
Grace Chung <gcw.chung@gmail.com>
2020-04-16 21:00:46 -0700

    Renamed file from time.js to marquee.js

---
+++ static/js/marquee.js
@@ -0,0 +1,17 @@
+"use strict";
+$(document).ready(function() {
+    const getDateTime = function() {
+        // Date object
+        const now = new Date().toString().toLowerCase();
+        
+        // Display on page
+        $(".marquee").text("⚠️ " + now);
+        $(".marquee").append("&nbsp; ▒&nbsp; United States".toLowerCase());
+        $(".marquee").append("&nbsp;  <b>Confirmed: </b>".toLowerCase());
+        $(".marquee").append(country_confirmed);
+        $(".marquee").append("&nbsp; <b>Deaths: </b>".toLowerCase());
+        $(".marquee").append(country_deaths + "&nbsp;⚠️");
+    }
+    // Update time every second
+    setInterval(getDateTime, 1000);
+});
