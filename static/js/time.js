068772f9403fc805cf993b703bffa64b7d404fa2
Grace Chung <gcw.chung@gmail.com>
2020-04-16 19:52:24 -0700

    Added jinja template variables into time.js

--- static/js/time.js
+++ static/js/time.js
@@ -3,10 +3,14 @@ $(document).ready(function() {
     const getDateTime = function() {
         // Date object
         const now = new Date().toString().toLowerCase();
-    
+        
         // Display on page
         $(".datetime").text(now);
-        
+        $(".datetime").append("   United States".toLowerCase());
+        $(".datetime").append("   Confirmed:".toLowerCase());
+        $(".datetime").append(country_confirmed);
+        $(".datetime").append("   Deaths:".toLowerCase());
+        $(".datetime").append(country_deaths);
     }
     // Update time every second
     setInterval(getDateTime, 1000);
