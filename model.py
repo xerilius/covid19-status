8da942cc836a10a14bfec8c17f0f09c5281bae76
Grace Chung <gcw.chung@gmail.com>
2020-05-17 12:29:09 -0700

    Removed unused code

--- model.py
+++ model.py
@@ -75,47 +75,6 @@ class Case(db.Model):
     active = db.Column(db.Integer, nullable=False)
 
 
-# class Confirmed(db.Model):
-#     """Confirmed number of people tested positive for COVID"""
-
-#     __tablename__ = "confirmed"
-
-#     confirmed_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     date = db.Column(db.Date, nullable=False)
-#     county_id = db.Column(db.Integer, db.ForeignKey('counties.county_id'), nullable=False)
-#     state_name = db.Column(db.String(64), nullable=True) 
-#     confirmed = db.Column(db.Integer, nullable=False)
-    
-#     # Association relationship for city
-#     county_confirmed = db.relationship("County", backref="confirmed")
-
-#     def __repr__(self):
-#         """Provides info when printed"""
-#         return "<Confirmed status_id={} status_date={} confirmed={}>".format(
-#             self.confirmed_id, self.date, self.confirmed
-#         )
- 
-
-# class Fatality(db.Model):
-#     """Information on fatalities from COVID"""
-
-#     __tablename__ = "fatalities"
-
-#     fatality_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-#     date = db.Column(db.Date, nullable=False)
-#     county_id = db.Column(db.Integer, 
-#                           db.ForeignKey('counties.county_id'), nullable=False)
-#     state_name = db.Column(db.String(64), nullable=True) 
-#     fatalities = db.Column(db.Integer, nullable=False)
-
-#     # Association relationship for city
-#     county_fatality = db.relationship("County", backref="fatalities")
-
-#     def __repr__(self):
-#         """Provides info when printed"""
-#         return "<Fatality fatality_id={} fatality_date={} fatalities={}>".format(self.fatality_id, self.date, self.fatalities)
-
-
 class Usa(db.Model):
     """Information on USA total confirmed & fatality numbers"""
 
